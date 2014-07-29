b49376d27371c574262be1e629586c3fb7131d72
Kshitij Parajuli <kshitijparajuli@gmail.com>
2014-07-28 22:39:40 -0400

    Add a bunch of pages
    - Add new estimate page mockup
    - Add project details page
    - Add story details page

--- asyncagile/poker/urls.py
+++ asyncagile/poker/urls.py
@@ -3,5 +3,8 @@ from poker import views
 from django.contrib.auth.decorators import login_required
 
 urlpatterns = patterns('',
-    url(r'story/view/(?P<pk>[-_\w]+)',  login_required(views.StoryDetails.as_view()), name='view-story'),
+    url(r'project/details/(?P<pk>[-_\w]+)',  login_required(views.ProjectDetails.as_view()), name='project-details'),
+    url(r'story/browse/$',  login_required(views.StoryBrowse.as_view()), name='story-browse'),
+    url(r'story/details/(?P<pk>[-_\w]+)',  login_required(views.StoryDetails.as_view()), name='story-details'),
+    url(r'estimate/new/(?P<story>\w+)',  login_required(views.EstimateNew.as_view()), name='estimate-new'),
 )
--- asyncagile/poker/views.py
+++ asyncagile/poker/views.py
@@ -1,18 +1,41 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.generic import TemplateView, DetailView, View
-from poker.models import Story
+from poker.models import Story, Estimate, Project
 
-def home(request):
-    return ''
-    if request.user and request.user.is_authenticated():
-        return redirect(reverse('view-projects'))
-    return render(request, 'landing.html')
+class StoryBrowse(TemplateView):
+    template_name = 'story/browse.html'
 
-class BaseView(TemplateView):
-    template_name = 'base.html'
 
 class StoryDetails(DetailView):
     model = Story
     context_object_name = 'story'
     template_name = 'story/details.html'
+
+
+class StoryVote(DetailView):
+    model = Story
+    context_object_name = 'story'
+    template_name = 'story/vote.html'
+
+
+class EstimateNew(TemplateView):
+    template_name = 'estimate/new.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(EstimateNew, self).get_context_data(**kwargs)
+        story = Story.objects.get(pk=kwargs['story'])
+        estimate = Estimate.objects.filter(user=self.request.user).filter(story=story)
+        if estimate:
+            # redirect
+            print "yes"
+
+        context['story'] = story
+
+        return context
+
+
+class ProjectDetails(DetailView):
+    template_name = 'project/details.html'
+    model = Project
+    context_object_name = 'project'
